456a079eb387e4be87ab6ba67b50f08a467826b3
Victor Canto <iamvictorcanto@gmail.com>
2022-04-03 22:52:29 -0300

    feat: added navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,14 +1,26 @@
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 // Generic Integration
 
 export function MarketingApp() {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate({ pathname: nextPathname }) {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 }
